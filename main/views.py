407f0acf19ad26366986e4d4130caf93d2c71ffc
ephraimphrase <ephraimphrase@gmail.com>
2022-07-19 20:13:10 +0100

    status changes

--- main/views.py
+++ main/views.py
@@ -35,10 +35,8 @@ def home(request):
     title = 'Home'
     count = Product.objects.all().count()
 
-    if Product.objects.get(product_status=True):
-        vert_count = Product.objects.get(product_status=True).count()
-    else:
-        vert_count = '0'
+    vert_count = Product.objects.get(product_status=True).count()
+
 
     verf_count = Product.objects.get(product_status=False).count()
     products = Product.objects.filter(Q(name__icontains=search_query) | Q(price__icontains=search_query) | Q(description__icontains=search_query), owner=request.user)
